a8f6cacc944604126ec99bccce1be46d615811e3
Navin <nvnsnha@gmail.com>
2026-02-22 21:13:36 +0000

    task1 and task2 py files

---
+++ task1.py
@@ -0,0 +1,11 @@
+num1,num2 = float(input("Enter the first number: ")),float(input("Enter the second number: "))
+
+Addition = num1 + num2
+Subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+
+print("Addition: ",Addition)
+print("Subtraction: ",Subtraction)
+print("Multiplication: ",Multiplication)
+print("Division: ",Division)
